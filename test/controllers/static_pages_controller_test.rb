33c25beb9baff103dde27dd22ec170dc202b0a19
yunlei <yunlei.liu@appfolio.com>
2014-10-18 16:51:45 -0700

    yl - use setup in test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "XmfunWeb"
+  end
+
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "XmfunWeb | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "XmfunWeb | About"
+    assert_select "title", "#{@base_title} | About"
   end
 
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "XmfunWeb | Home"
+    assert_select "title", "#{@base_title} | Home"
   end
 end
