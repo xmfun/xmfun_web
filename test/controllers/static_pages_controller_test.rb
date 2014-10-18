607a359566a859c0be6a0b7d79e22ca7507fe858
yunlei <yunlei.liu@appfolio.com>
2014-10-18 16:46:15 -0700

    yl - add title for static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "XmfunWeb | Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "XmfunWeb | About"
   end
 
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "XmfunWeb | Home"
   end
 
 end
