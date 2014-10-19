f71fb1cae8559f6ae942f5d8983e99fb8514886e
yunlei <yunlei.liu@appfolio.com>
2014-10-19 00:20:24 -0700

    yl - add bootstrap and set up home css styling

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", @base_title
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title} | Contact"
+  end
 end
