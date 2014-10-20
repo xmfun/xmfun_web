6510fa543bc1740782accb6d50b3bb506d56df0e
yunlei <yunlei.liu@appfolio.com>
2014-10-19 23:16:56 -0700

    yl - add admin column in user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,4 +38,28 @@ class UsersControllerTest < ActionController::TestCase
     get :index
     assert_redirected_to login_url
   end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              "wowowo",
+                                            password_confirmation: "wowowo",
+                                            admin: true }
+    assert_not @other_user.admin?
+  end
 end
