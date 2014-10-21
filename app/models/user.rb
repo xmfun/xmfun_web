3230e234c4016007ef0b1351e7deb4d18ee1d519
yunlei <yunlei.liu@appfolio.com>
2014-10-21 01:12:57 -0700

    yl - add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
 
@@ -24,12 +24,21 @@ class User < ActiveRecord::Base
   end
 
   def activate
-    update_attribute(:activated,    true)
-    update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver!
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+     update_columns(reset_digest:  User.digest(reset_token),
+                    reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver!
   end
 
   def self.digest(string)
@@ -44,6 +53,10 @@ class User < ActiveRecord::Base
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,11 +15,14 @@ class UserMailerTest < ActionMailer::TestCase
   end
 
   test "password_reset" do
-    mail = UserMailer.password_reset
+    user = users(:foo)
+    user.create_reset_digest
+    mail = UserMailer.password_reset(user)
     assert_equal "Password reset", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [user.email], mail.to
     assert_equal ["noreply@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match user.reset_token,        mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
   end
 
 end
